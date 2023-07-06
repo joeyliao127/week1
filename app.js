7bc85933d480392e1755f5bd7597bba18d21f6ac
Joey Liao <joey.liao0127@gmail.com>
2023-07-06 10:25:18 +0800

    bug fix

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const topItems = document.querySelector(".top-items");
 const body = document.querySelector("body");
-console.log(hamburger);
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
 hamburger.addEventListener("click", (e) => {
   e.stopPropagation();
   topItems.style.display = "block";
@@ -9,15 +10,19 @@ hamburger.addEventListener("click", (e) => {
   body.style.overflow = "hidden";
 });
 
-body.addEventListener("click", (e) => {
-  if (!e.target.closest(".hamburger")) {
-    body.classList.remove("no-scroll");
-    body.style.overflow = "auto";
-  }
-  topItems.style.display = "none";
-});
+if (windowWidth > 360 && windowWidth < 600) {
+  body.addEventListener("click", (e) => {
+    if (!e.target.closest(".hamburger")) {
+      body.classList.remove("no-scroll");
+      body.style.overflow = "auto";
+    }
+    topItems.style.display = "none";
+  });
 
-topItems.addEventListener("click", (e) => {
-  e.stopPropagation();
-  topItems.style.display = "block";
-});
+  topItems.addEventListener("click", (e) => {
+    e.stopPropagation();
+    topItems.style.display = "block";
+  });
+}
+
+console.log(window);
